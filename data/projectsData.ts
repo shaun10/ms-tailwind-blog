ae0536684ae2a7b770abb6e461a9b7cbc2c81aa3
shaun <shaun@gatech.edu>
2025-07-11 22:07:15 -0700

    update projects and some js.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Trillion Trees Movement',
+    description: `World Economic Forum's 1 Trillion Trees initiative`,
+    imgSrc: '/static/images/1t.avif',
+    href: 'https://www.1t.org/home',
   },
 ]
 
